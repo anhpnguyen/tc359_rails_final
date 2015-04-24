03446ac5c5fdb4b39a55cacd026ac4a3f6dd2c11
Anh <anhpnguyen.an@gmail.com>
2015-04-24 17:06:15 -0400

    fix views

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,10 +1,10 @@
 class Story < ActiveRecord::Base
   validates_presence_of :subject, :note
-  # def self.search(search)
-  #   if search
-  #     find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-  #   # else
-  #     # find(:all)
-  #    end
-  # end
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+     end
+  end
 end
