784fd34e101cf4ba4677f1e763329c5d84ec81b1
Anh <anhpnguyen.an@gmail.com>
2015-04-25 15:46:42 -0400

    search works, comments get displayed due to missing = to output results

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,9 +2,9 @@ class Story < ActiveRecord::Base
   validates_presence_of :subject, :note
   def self.search(search)
     if search
-      find(:all, :conditions => ['name LIKE ?', "%#{search.to_s}%"])
+      where('subject LIKE ?', "%#{search}%")
     else
-      find(:all)
+      all
      end
   end
 end
